c93a6b50ddfc84b8ecdba8f8679879181fa38b88
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-04-28 15:30:24 +0100

    rock,paper,scissors

---
+++ main.js
@@ -0,0 +1,14 @@
+const PLAYER_ROCK = "rock";
+const PLAYER_PAPER = "paper";
+const PLAYER_SCISSORS = "scissors";
+
+const COMPUTER_MOVE_ROCK = "rock";
+const COMPUTER_MOVE_PAPER = "paper";
+const COMPUTER_MOVE_SCISSORS ="scissors";
+
+let playerMove = PLAYER_ROCK
+let computerMove = COMPUTER_MOVE_PAPER
+
+if(playerMove === computerMove){
+    console.log("Its a draw!!");
+} else if ()
