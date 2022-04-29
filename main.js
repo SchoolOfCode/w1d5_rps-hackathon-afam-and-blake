2c4cfa4f3912589eba4c3358f3b782b355500735
AfamOkoh <afamokoh@gmail.com>
2022-04-29 10:21:53 +0100

    planned, executed and tested task 1

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
-const PLAYER_ROCK = "rock";
-const PLAYER_PAPER = "paper";
-const PLAYER_SCISSORS = "scissors";
+/*
+Task 1
+- set of if statements to determine rock paper scissors winner
+- The two moves stored in variables
+- Hard code each move in these variables
+- Complete when all permutationss of the combined moves have been handled.
 
-const COMPUTER_MOVE_ROCK = "rock";
-const COMPUTER_MOVE_PAPER = "paper";
-const COMPUTER_MOVE_SCISSORS ="scissors";
+- Create constants with each move 
+- Create variables for player and computer move
 
-let playerMove = PLAYER_ROCK
-let computerMove = COMPUTER_MOVE_PAPER
+Create an if statement with permutations below
+Draw    
+   if cMove = Rock and Pmove = Rock, 
+    if cMove = Paper and Pmove = Paper,
+    if cMove = Scissors and Pmove = Scissors
 
-if(playerMove === computerMove){
-    console.log("Its a draw!!");
-} else if ()
+
+    Player Wins 
+     if pMove = Rock and Cmove = Scissors or 
+     if pMove = Paper and Cmove = Rock or
+     if pMove = Scissors and Cmove = Paper
+
+  Computer Wins (don't need to include this)
+    if cMove = Rock and Pmove = Scissors, 
+    if cMove = Paper and Pmove = Rock,
+    if cMove = Scissors and Pmove = Paper
+
+
+*/
+
+const ROCK = "rock";
+const PAPER = "paper";
+const SCISSORS = "scissors";
+
+let playerMove = PAPER;
+let computerMove = ROCK;
+
+if (playerMove === computerMove) {
+  console.log("Its a draw!!");
+} else if (
+  (playerMove === ROCK && computerMove === SCISSORS) ||
+  (playerMove === PAPER && computerMove === ROCK) ||
+  (playerMove === SCISSORS && computerMove === PAPER)
+) {
+  console.log("Player Wins!");
+} else {
+  console.log("Computer Wins!");
+}
